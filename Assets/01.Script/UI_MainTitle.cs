dbff0386320d3a2b44252a61179eb173d8cd2d44
lmp88 <tjemapdlwl@gmail.com>
2021-07-12 22:09:24 +0900

    Main Tile 작성 중....01

---
+++ Assets/01.Script/UI_MainTitle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_MainTitle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void btnNewGame()
+    {
+
+    }
+
+    public void btnConnectGame()
+    {
+
+    }
+
+    public void btnExit()
+    {
+        
+    }
+}
